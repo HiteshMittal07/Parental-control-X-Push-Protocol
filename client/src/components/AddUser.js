52efe57f146561383337e90cf01992e5566255bd
Hitesh <smartyboy.hm15@gmail.com>
2024-01-13 14:32:47 +0530

    setting up routing

--- client/src/components/AddUser.js
+++ client/src/components/AddUser.js
@@ -1,17 +1,3 @@
-<<<<<<< HEAD
-import React, { useContext } from "react";
-import { ParentalContext } from "../ParentalContext";
-import { toast } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
-export default function AddUser() {
-  const { state2 } = useContext(ParentalContext);
-  async function Adduser() {
-    const { contract2 } = state2;
-    const address = document.querySelector("#address").value;
-    try {
-      const tx = await contract2.addUser(address);
-      await tx.wait();
-=======
 import React, { useContext, useState } from "react";
 import { ParentalContext } from "../ParentalContext";
 import { toast } from "react-toastify";
@@ -55,31 +41,11 @@ export default function AddUser() {
         },
         toast.success("send successfully")
       );
->>>>>>> 09d64b6 (adding push notifications)
     } catch (error) {
       toast.error(error.reason);
     }
     toast.success("User added Successfully");
   }
-<<<<<<< HEAD
-  return (
-    <div>
-      <h1 className="text-center mt-5">Add User</h1>
-      <form
-        onSubmit={Adduser}
-        className="d-flex flex-column align-items-center"
-      >
-        <input
-          type="text"
-          id="address"
-          placeholder="Enter user's wallet address"
-        />
-        <br />
-        <button onClick={Adduser} className="btn btn-primary col-4 mt-3">
-          Add User
-        </button>
-      </form>
-=======
 
   const [showModal, setShowModal] = useState(false);
 
@@ -152,7 +118,6 @@ export default function AddUser() {
           </div>
         </>
       )}
->>>>>>> 09d64b6 (adding push notifications)
     </div>
   );
 }
--- client/src/components/Owners.js
+++ client/src/components/Owners.js
@@ -2,59 +2,6 @@ import { useState, useEffect, useContext } from "react";
 import "bootstrap/dist/css/bootstrap.css";
 import Axios from "axios";
 import { ParentalContext } from "../ParentalContext";
-<<<<<<< HEAD
-
-const Owners = () => {
-  const { state2 } = useContext(ParentalContext);
-  const { contractRead2 } = state2;
-  const [owners, setOwners] = useState([]);
-
-  useEffect(() => {
-    const fetchOwnersDetails = async () => {
-      const tx = await contractRead2.getOwners();
-      const ownersWithDetails = await Promise.all(
-        tx.map(async (ownerAddress) => {
-          const { data } = await Axios.get("https://randomuser.me/api/");
-          const details = data.results[0];
-          return { ownerAddress, details };
-        })
-      );
-      setOwners(ownersWithDetails);
-    };
-
-    contractRead2 && fetchOwnersDetails();
-  }, [contractRead2]);
-
-  return (
-    <div className="container mt-4">
-      <h2 className="text-uppercase mb-3 text-light">Owners</h2>
-      <div className="row">
-        {owners.map(({ ownerAddress, details }) => (
-          <div className="col-md-6 mb-4" key={ownerAddress}>
-            <div className="card">
-              <img
-                src={details.picture?.large} // Use larger images
-                className="card-img-top img-fluid rounded"
-                alt="Owner"
-                style={{ objectFit: "cover", height: "400px" }} // Adjust height
-              />
-              <div className="card-body">
-                <h5 className="card-title">
-                  {details.name?.first} {details.name?.last}
-                </h5>
-                <p className="card-text">
-                  <span className="fw-bold text-primary">Address:</span>{" "}
-                  {ownerAddress}
-                </p>
-                <a href="#" className="btn btn-primary">
-                  Know More
-                </a>
-              </div>
-            </div>
-          </div>
-        ))}
-      </div>
-=======
 import { ethers } from "ethers";
 import abi from "../contractJson/Parental.json";
 import BeatLoader from "react-spinners/BeatLoader";
@@ -143,7 +90,6 @@ const Owners = () => {
           </div>
         </div>
       )}
->>>>>>> 09d64b6 (adding push notifications)
     </div>
   );
 };
