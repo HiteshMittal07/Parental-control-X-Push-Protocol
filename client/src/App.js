bf0501c7d7ecaa24a69763b42345b934d07fb34b
HiteshMittal07 <smartyboy.hm15@gmail.com>
2024-02-14 18:42:52 +0530

    Bug fixing in joining

---
+++ client/src/App.js
@@ -0,0 +1,156 @@
+// import * as PushSDK from "@pushprotocol/restapi";
+import { BigNumber, ethers } from "ethers";
+import { useState } from "react";
+import { PushAPI, CONSTANTS } from "@pushprotocol/restapi";
+
+import "bootstrap/dist/css/bootstrap.css";
+import Header from "./components/Header";
+import Router from "./routes/Router";
+import abi from "./contractJson/CreateWallet.json";
+import abi2 from "./contractJson/push.json";
+import "./App.css";
+import { ParentalContext } from "./ParentalContext";
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import NotificationInterface from "./NotificationInterface";
+import CreateWallet from "./components/CreateWallet";
+import JoinWallet from "./components/JoinWallet";
+import Initial from "./components/Initial";
+function App() {
+  const [state, setState] = useState({
+    provider: null,
+    signer: null,
+    contract: null,
+    contractRead: null,
+  });
+  const [created, SetCreated] = useState(null);
+  const [connected, setConnected] = useState(false);
+  const [joined, SetJoined] = useState(null);
+  const [account, setAccount] = useState("not connected");
+  const [contractAddress, setContractAddress] = useState(null);
+  const [owner, setOwner] = useState(null);
+  const connectWallet = async () => {
+    const contractAddress = "0x384cc0998C42FAb018Bf622171902261A7633937";
+    const contractABI = abi.abi;
+    try {
+      const { ethereum } = window;
+      let provider = new ethers.providers.Web3Provider(ethereum);
+      const selectedValue = 1442;
+      await provider.send("wallet_switchEthereumChain", [
+        { chainId: `0x${Number(selectedValue).toString(16)}` },
+      ]);
+      const accounts = await ethereum.request({
+        method: "eth_requestAccounts",
+      });
+
+      let signer = provider.getSigner();
+      let contract = new ethers.Contract(contractAddress, contractABI, signer);
+      let contractRead = new ethers.Contract(
+        contractAddress,
+        contractABI,
+        provider
+      );
+
+      ethereum.on("accountsChanged", async () => {
+        const updatedAccounts = await ethereum.request({
+          method: "eth_requestAccounts",
+        });
+        setAccount(updatedAccounts);
+        provider = new ethers.providers.Web3Provider(ethereum);
+        signer = provider.getSigner();
+        contract = new ethers.Contract(contractAddress, contractABI, signer);
+        contractRead = new ethers.Contract(
+          contractAddress,
+          contractABI,
+          provider
+        );
+        setState({ provider, signer, contract, contractRead });
+      });
+
+      setAccount(accounts);
+      setState({ provider, signer, contract, contractRead });
+      setConnected(true);
+      toast.success("Connected successfully");
+    } catch (error) {
+      alert(error);
+    }
+  };
+
+  async function send() {
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    const signer = provider.getSigner();
+    const userAlice = await PushAPI.initialize(signer, {
+      env: CONSTANTS.ENV.STAGING,
+    });
+    // const pushChannelAddress = "0x031476AA8fca4F61f9383D14a631AAdb5f530982";
+    // await userAlice.channel.send(
+    //   ["*"],
+    //   {
+    //     notification: {
+    //       title: "Hey there",
+    //       body: "Web3 native notification!",
+    //     },
+    //   },
+    //   toast.success("send successfully")
+    // );
+    // console.log(userAlice.channel);
+    // const info = await userAlice.channel.info();
+    const aliceSubscriptions = await userAlice.notification.subscriptions();
+    console.log(aliceSubscriptions);
+  }
+  const sendNotificationWithDelegate = async () => {
+    // const delegateAddress = 'eip155:11155111:0xA1897451FC8A83aaF66d5303729C6220cf415a2c';
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    const delegateSigner = provider.getSigner();
+    console.log(delegateSigner);
+    const userAlice = await PushAPI.initialize(delegateSigner, {
+      env: CONSTANTS.ENV.STAGING,
+      filter: {
+        channels: ["0x11ae45Ab10039D1EA50A54edd2638200fa3aFaEa"],
+      },
+      account: "0x11ae45Ab10039D1EA50A54edd2638200fa3aFaEa",
+    });
+    // console.log(userAlice.channel);
+    //   const info=await userAlice.channel.info();
+    // console.log(info);
+    const sendNotificationResponse = await userAlice.channel.send(
+      ["0x11ae45Ab10039D1EA50A54edd2638200fa3aFaEa"],
+      {
+        notification: {
+          title: "Hey there",
+          body: "Web3 native notification!",
+        },
+      }
+    );
+    const inboxNotifications = await userAlice.notification.list("INBOX");
+    console.log(inboxNotifications);
+    // console.log('Notification sent successfully:', sendNotificationResponse);
+  };
+
+  return (
+    <ParentalContext.Provider
+      value={{
+        state,
+        setState,
+        connectWallet,
+        contractAddress,
+        setContractAddress,
+        SetCreated,
+        SetJoined,
+        joined,
+        created,
+        connected,
+        owner,
+        setOwner,
+      }}
+    >
+      <div className="App">
+        <Header />
+        {/* <button onClick={send}>send</button> */}
+        <Router />
+      </div>
+    </ParentalContext.Provider>
+  );
+}
+
+export default App;
---
+++ client/src/components/Home.js
@@ -0,0 +1,416 @@
+import { ethers } from "ethers";
+import { useState, useEffect, useContext } from "react";
+import "bootstrap/dist/css/bootstrap.css";
+import "./Home.css";
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { ParentalContext } from "../ParentalContext";
+import abi from "../contractJson/Parental.json";
+import BeatLoader from "react-spinners/BeatLoader";
+import { PushAPI, CONSTANTS } from "@pushprotocol/restapi";
+export const Home = () => {
+  const [balance, setBalance] = useState(null);
+  const [transactionCount, setTransactionCount] = useState(0);
+  const { contractAddress, owner } = useContext(ParentalContext);
+  const contractABI = abi.abi;
+  const [loading, setLoading] = useState(false);
+  const override = {
+    display: "block",
+    marginTop: "200px",
+  };
+  const switchChain3 = async () => {
+    window.ethereum.on("chainChanged", Deposit);
+
+    let provider = new ethers.providers.Web3Provider(window.ethereum);
+    const selectedValue = 1442;
+    // Send the chain switch request
+    provider.send("wallet_switchEthereumChain", [
+      { chainId: `0x${Number(selectedValue).toString(16)}` },
+    ]);
+    if (window.ethereum.chainId == `0x${Number(selectedValue).toString(16)}`) {
+      await Deposit();
+    }
+    // console.log(window.ethereum.chainId);
+  };
+  const Deposit = async () => {
+    let provider = new ethers.providers.Web3Provider(window.ethereum);
+    let signer = provider.getSigner();
+    let contractRead2 = new ethers.Contract(
+      contractAddress,
+      contractABI,
+      provider
+    );
+    let contract = contractRead2.connect(signer);
+    contractRead2.on("Deposit", async (sender, amount, event) => {
+      window.ethereum.removeListener("chainChanged", Deposit);
+      await provider.send("wallet_switchEthereumChain", [
+        { chainId: "0xAA36A7" },
+      ]);
+      const userAlice = await PushAPI.initialize(signer, {
+        env: CONSTANTS.ENV.STAGING,
+        filter: {
+          channels: [`${owner}`],
+        },
+        account: `${owner}`,
+      });
+      const a = parseInt(amount) / Math.pow(10, 18);
+      await userAlice.channel.send(["*"], {
+        notification: {
+          title: `Deposited by ${sender}`,
+          body: `${a} ETH`,
+        },
+      });
+      toast.success("Deposit successfully");
+      event.removeListener();
+    });
+    const amount = document.querySelector("#amount").value;
+    const option = { value: ethers.utils.parseEther(amount) };
+    const tx = await contract.DepositEth(option);
+    await tx.wait();
+  };
+
+  useEffect(() => {
+    const switchChain = async () => {
+      try {
+        setLoading(true);
+        let provider = new ethers.providers.Web3Provider(window.ethereum);
+        const selectedValue = 1442;
+        await provider.send("wallet_switchEthereumChain", [
+          { chainId: `0x${Number(selectedValue).toString(16)}` },
+        ]);
+        setLoading(false);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    switchChain();
+  }, []);
+
+  const switchChain = async () => {
+    window.ethereum.on("chainChanged", getBalance);
+
+    let provider = new ethers.providers.Web3Provider(window.ethereum);
+    const selectedValue = 1442;
+    // Send the chain switch request
+    provider.send("wallet_switchEthereumChain", [
+      { chainId: `0x${Number(selectedValue).toString(16)}` },
+    ]);
+    if (window.ethereum.chainId == `0x${Number(selectedValue).toString(16)}`) {
+      await getBalance();
+    }
+    console.log(window.ethereum.chainId);
+  };
+  const getBalance = async () => {
+    let provider = new ethers.providers.Web3Provider(window.ethereum);
+    let contractRead2 = new ethers.Contract(
+      contractAddress,
+      contractABI,
+      provider
+    );
+    console.log(contractRead2);
+    const tx1 = await contractRead2.getBalance();
+    window.ethereum.removeListener("chainChanged", getBalance);
+    const num = parseInt(tx1) / Math.pow(10, 18);
+    setBalance(num);
+  };
+
+  const owner2 = owner;
+  const switchChain2 = async () => {
+    window.ethereum.on("chainChanged", submitTx);
+
+    let provider = new ethers.providers.Web3Provider(window.ethereum);
+    const selectedValue = 1442;
+    // Send the chain switch request
+    provider.send("wallet_switchEthereumChain", [
+      { chainId: `0x${Number(selectedValue).toString(16)}` },
+    ]);
+    if (window.ethereum.chainId == `0x${Number(selectedValue).toString(16)}`) {
+      await submitTx();
+    }
+    console.log(window.ethereum.chainId);
+  };
+  const submitTx = async () => {
+    // event.preventDefault();
+    let provider = new ethers.providers.Web3Provider(window.ethereum);
+    const selectedValue = 1442;
+    await provider.send("wallet_switchEthereumChain", [
+      { chainId: `0x${Number(selectedValue).toString(16)}` },
+    ]);
+    let signer = provider.getSigner();
+    let contractRead2 = new ethers.Contract(
+      contractAddress,
+      contractABI,
+      provider
+    );
+    let contract = contractRead2.connect(signer);
+    contractRead2.on(
+      "SubmitTrans",
+      async (owner, txIndex, to, value, event) => {
+        window.ethereum.removeListener("chainChanged", submitTx);
+        await provider.send("wallet_switchEthereumChain", [
+          { chainId: "0xAA36A7" },
+        ]);
+        const userAlice = await PushAPI.initialize(signer, {
+          env: CONSTANTS.ENV.STAGING,
+          filter: {
+            channels: [`${owner2}`],
+          },
+          account: `${owner2}`,
+        });
+        const a = parseInt(value) / Math.pow(10, 18);
+        await userAlice.channel.send(["*"], {
+          notification: {
+            title: `Submitted by ${owner}`,
+            body: `To:${to} Value:${a}`,
+          },
+        });
+        toast.success("Submitted successfully");
+        event.removeListener();
+      }
+    );
+    const address = document.querySelector("#subAddress").value;
+    const value = document.querySelector("#subValue").value;
+    const amount = ethers.utils.parseEther(value);
+    const message = document.querySelector("#message").value;
+    try {
+      const tx2 = await contract.SubmitTransaction(address, amount, message);
+      await tx2.wait();
+    } catch (error) {
+      toast.error(error.reason);
+      // console.log(error);
+    }
+  };
+
+  const switchChain4 = async () => {
+    window.ethereum.on("chainChanged", confirmTx);
+
+    let provider = new ethers.providers.Web3Provider(window.ethereum);
+    const selectedValue = 1442;
+    // Send the chain switch request
+    provider.send("wallet_switchEthereumChain", [
+      { chainId: `0x${Number(selectedValue).toString(16)}` },
+    ]);
+    if (window.ethereum.chainId == `0x${Number(selectedValue).toString(16)}`) {
+      await confirmTx();
+    }
+    // console.log(window.ethereum.chainId);
+  };
+  const confirmTx = async () => {
+    let provider = new ethers.providers.Web3Provider(window.ethereum);
+    let signer = provider.getSigner();
+    let contractRead2 = new ethers.Contract(
+      contractAddress,
+      contractABI,
+      provider
+    );
+    let contract = contractRead2.connect(signer);
+    contractRead2.on("ConfirmTrans", async (owner, txIndex, event) => {
+      window.ethereum.removeListener("chainChanged", confirmTx);
+      await provider.send("wallet_switchEthereumChain", [
+        { chainId: "0xAA36A7" },
+      ]);
+      const userAlice = await PushAPI.initialize(signer, {
+        env: CONSTANTS.ENV.STAGING,
+        filter: {
+          channels: [`${owner2}`],
+        },
+        account: `${owner2}`,
+      });
+      await userAlice.channel.send(["*"], {
+        notification: {
+          title: `Confirmed`,
+          body: `By:${owner} Transaction Index:${txIndex}`,
+        },
+      });
+      toast.success("Transaction confirmed successfully");
+      event.removeListener();
+    });
+    const txIndex = document.querySelector("#index").value;
+    try {
+      const tx3 = await contract.ConfirmTransactions(txIndex);
+      await tx3.wait();
+    } catch (error) {
+      toast.error(error.reason);
+    }
+  };
+
+  // useEffect(() => {
+  //   let provider = new ethers.providers.Web3Provider(window.ethereum);
+  //       let contractRead2=new ethers.Contract(contractAddress,contractABI,provider);
+  //   const fetchTransactionCount = async () => {
+  //     const count = await contractRead2.getTransactionCount();
+  //     setTransactionCount(parseInt(count));
+  //   };
+  //   contractRead2 && fetchTransactionCount();
+  // }, []);
+
+  return (
+    <div>
+      {loading ? (
+        <>
+          <BeatLoader color="#ffffff" cssOverride={override} />
+          <p className="text-light">Wait Switching Network...</p>
+        </>
+      ) : (
+        <div
+          className="home-container container mt-5"
+          style={{ backgroundColor: "rgba(196, 164, 162, 0.5)" }}
+        >
+          <div className="row">
+            {/* Balance Checker */}
+            <div className="col-md-6">
+              <div
+                className="balance-box"
+                style={{
+                  //   backgroundColor: "rgba(196, 164, 162)",
+                  padding: "20px",
+                  marginBottom: "20px",
+                  height: "400px",
+                }}
+              >
+                <h2 className="section-title">Balance Checker</h2>
+                <div className="balance-info">
+                  <h3 className="balance-text">Balance: {balance} ETH</h3>
+                  <button onClick={switchChain} className="btn btn-primary">
+                    Get Balance
+                  </button>
+                </div>
+                {/* Tagline */}
+                <div
+                  className="tagline"
+                  style={{
+                    position: "absolute",
+                    bottom: "20px",
+                    left: "50%",
+                    transform: "translateX(-50%)",
+                    textAlign: "center",
+                    width: "80%",
+                  }}
+                >
+                  <p
+                    style={{
+                      fontFamily: "Arial, sans-serif",
+                      fontSize: "19px",
+                      fontStyle: "italic",
+                      color: "#333",
+                    }}
+                  >
+                    "Parental Control is a smart contract-driven system that
+                    monitors and logs financial transactions initiated by
+                    children. It mandates explicit parental or guardian approval
+                    for any payment execution, enhancing control over minors'
+                    spending. This solution offers comprehensive oversight for
+                    effective management of children's financial activities."
+                  </p>
+                </div>
+              </div>
+            </div>
+
+            {/* Deposit Amount */}
+            <div className="col-md-6">
+              <div
+                className="deposit-box"
+                style={{
+                  //   backgroundColor: "rgba(196, 164, 162)",
+                  padding: "20px",
+                  marginBottom: "20px",
+                  height: "400px",
+                }}
+              >
+                <h2 className="section-title">Deposit Amount</h2>
+                <div className="input-group mb-3">
+                  <input
+                    type="text"
+                    id="amount"
+                    placeholder="Enter the Deposit Amount"
+                    className="form-control"
+                  />
+                </div>
+                <div className="input-group mb-3">
+                  <button className="btn btn-danger" onClick={switchChain3}>
+                    Deposit
+                  </button>
+                </div>
+              </div>
+            </div>
+
+            {/* Submit a Transaction */}
+            <div className="col-md-6">
+              <div
+                className="submit-box"
+                style={{
+                  //   backgroundColor: "rgba(196, 164, 162)",
+                  padding: "20px",
+                  marginBottom: "10px",
+                  height: "400px",
+                }}
+              >
+                <h2 className="section-title">Submit a Transaction</h2>
+                <div className="input-group mb-2">
+                  <input
+                    type="text"
+                    id="subAddress"
+                    placeholder="Enter the Address"
+                    className="form-control"
+                  />
+                </div>
+                <div className="input-group mb-2">
+                  <input
+                    type="text"
+                    id="subValue"
+                    placeholder="Enter the Amount"
+                    className="form-control"
+                  />
+                </div>
+                <div className="input-group mb-2">
+                  <textarea
+                    id="message"
+                    placeholder="Enter the Message"
+                    className="form-control"
+                    rows={3}
+                    cols={50}
+                  />
+                </div>
+                <div className="input-group mb-3">
+                  <button className="btn btn-danger" onClick={switchChain2}>
+                    Submit
+                  </button>
+                </div>
+              </div>
+            </div>
+
+            {/* Confirm a Transaction */}
+            <div className="col-md-6">
+              <div
+                className="confirm-box"
+                style={{
+                  //   backgroundColor: "rgba(196, 164, 162)",
+                  padding: "20px",
+                  marginBottom: "20px",
+                  height: "400px",
+                }}
+              >
+                <h2 className="section-title">Confirm a Transaction</h2>
+                <div className="input-group mb-3">
+                  <input
+                    type="text"
+                    id="index"
+                    placeholder="Enter the Transaction no."
+                    className="form-control"
+                  />
+                </div>
+                <div className="input-group mb-3">
+                  <button className="btn btn-primary" onClick={switchChain4}>
+                    Confirm
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Home;
