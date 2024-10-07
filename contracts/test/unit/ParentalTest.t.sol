2cd4aa5ad425b10f246a53750c03a0a9767a9619
HiteshMittal07 <smartyboy.hm15@gmail.com>
2024-10-07 21:23:46 +0530

    refactor: some more unit test functions

--- contracts/test/unit/ParentalTest.t.sol
+++ contracts/test/unit/ParentalTest.t.sol
@@ -9,6 +9,9 @@ contract ParentalTest is Test {
     Parental parental;
     address PARENT = makeAddr("parent");
     address CHILD = makeAddr("child");
+    address TO = makeAddr("to");
+    address THIRD_PERSON = makeAddr("thirdPerson");
+    uint256 AMOUNT_TO_SEND = 1 ether;
     bytes32 private constant CHILD_ROLE = keccak256("CHILD_ROLE");
     bytes32 private constant PARENT_ROLE = keccak256("PARENT_ROLE");
 
@@ -22,6 +25,7 @@ contract ParentalTest is Test {
         assertEq(parental.hasRole(PARENT_ROLE, PARENT), true);
     }
 
+    // testing depositing eth to the contract
     function testDepositEth() external {
         vm.startPrank(PARENT);
         parental.depositEth{value: 1 ether}();
@@ -29,6 +33,7 @@ contract ParentalTest is Test {
         vm.stopPrank();
     }
 
+    // testing withdrawing eth from the contract, with different reverts.
     function testWithdrawEth() external {
         vm.startPrank(PARENT);
         parental.depositEth{value: 2 ether}();
@@ -72,13 +77,69 @@ contract ParentalTest is Test {
         parental.addChild(address(1));
         vm.expectRevert(Parental.AlreadyHavingChildRole.selector);
         parental.addParent(address(1));
+
+        vm.stopPrank();
     }
 
     function testRemoveChild() external {
         vm.startPrank(PARENT);
         parental.addChild(CHILD);
 
+        // checking if child role is revoked after removal.
         parental.removeChild(CHILD);
         assertEq(parental.hasRole(CHILD_ROLE, CHILD), false);
+        
+        vm.expectRevert(Parental.ChildDoesNotExist.selector);
+        parental.removeChild(CHILD);
+
+        vm.expectRevert(Parental.InvalidAddress.selector);
+        parental.removeChild(address(0));
+        vm.stopPrank();
+    }
+
+    // testing removing parent from contract, tested removal of first parent by other parents and expected reverts in this case.
+    function testRemoveParent() external {
+        vm.startPrank(PARENT);
+        address PARENT2 = makeAddr("parent2");
+        parental.addParent(PARENT2);
+
+        // checking if parent2 role is revoked from removal or not.
+        parental.removeParent(PARENT2);
+        assertEq(parental.hasRole(PARENT_ROLE, PARENT2), false);
+
+        // after removal of parent2, if we again try to remove parent2, it should revert.
+        vm.expectRevert(Parental.ParentDoesNotExist.selector);
+        parental.removeParent(PARENT2);
+        assertEq(parental.hasRole(PARENT_ROLE, PARENT2), false);
+
+        // if we try to remove to address 0, it should revert.
+        vm.expectRevert(Parental.InvalidAddress.selector);
+        parental.removeParent(address(0));
+
+        parental.addParent(PARENT2);
+        vm.stopPrank();
+
+        // if parent2 tries to remove parent1, it should revert as no one can remove parent1.
+        vm.startPrank(PARENT2);
+        vm.expectRevert(Parental.CannotRemoveFirstParent.selector);
+        parental.removeParent(PARENT);
+
+        vm.stopPrank();
+    }
+
+    function testSubmitTransaction() external {
+        vm.startPrank(PARENT);
+        parental.addChild(CHILD);
+        parental.depositEth{value: 2 ether}();
+
+        vm.stopPrank();
+        vm.startPrank(CHILD);
+        parental.submitTransaction(TO, AMOUNT_TO_SEND);
+        assertEq(parental.getTransaction().length, 1);
+        vm.stopPrank();
+
+        vm.prank(THIRD_PERSON);
+        vm.expectRevert(Parental.InsufficientRights.selector);
+        parental.submitTransaction(TO, AMOUNT_TO_SEND);
     }
 }
