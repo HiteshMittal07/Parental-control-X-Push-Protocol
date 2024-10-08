782acb6a03a80c7d44420f528f22d1a69a372bad
HiteshMittal07 <smartyboy.hm15@gmail.com>
2024-10-08 16:21:52 +0530

    feat: Intialised Integration testing

---
+++ contracts/test/integration/ParentalIntegration.t.sol
@@ -0,0 +1,33 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+import {Test, console} from "forge-std/Test.sol";
+import {Vm} from "forge-std/Vm.sol";
+import {DeployCreateWallet} from "../../script/DeployCreateWallet.s.sol";
+import {CreateWallet} from "../../src/CreateWallet.sol";
+
+contract ParentalIntegrationTest is Test {
+    CreateWallet createWallet;
+
+    function setUp() public {
+        DeployCreateWallet deployer = new DeployCreateWallet();
+        createWallet = deployer.run();
+    }
+
+    function testWallet() public {
+        vm.recordLogs();
+
+        // Call the function to test
+        createWallet.CreateParentalWallet();
+        console.log(createWallet.getParentalWalletAddress());
+        // Fetch the logs after the function call
+        Vm.Log[] memory logs = vm.getRecordedLogs();
+
+        // Make sure at least one log is recorded
+        // assertEq(logs.length, 1, "No events were emitted");
+
+        // Decode the first log as the 'created' event
+
+        console.log(address(logs[0].emitter));
+    }
+}
