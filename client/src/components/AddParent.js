df56932461cbcd715402616ba796b051e0891647
HiteshMittal07 <smartyboy.hm15@gmail.com>
2024-10-28 16:00:34 +0530

    feat: remove parents option added

--- client/src/components/AddParent.js
+++ client/src/components/AddParent.js
@@ -13,7 +13,7 @@ export default function AddOwner() {
   const owner = localStorage.getItem("owner");
   async function addOwner() {
     let provider = getWeb3Provider();
-    const selectedValue = "2442";
+    const selectedValue = "534351";
     await switchNetwork(selectedValue);
     let signer = provider.getSigner();
     let contractRead2 = getParentalContractRead(provider, contractAddress);
@@ -36,6 +36,33 @@ export default function AddOwner() {
     }
     toast.success("Parent added Successfully");
   }
+  async function removeOwner() {
+    let provider = getWeb3Provider();
+    const selectedValue = "534351";
+    await switchNetwork(selectedValue);
+    let signer = provider.getSigner();
+    let contractRead2 = getParentalContractRead(provider, contractAddress);
+    let contract = contractRead2.connect(signer);
+    const address = document.querySelector("#address").value;
+    try {
+      const tx = await contract.removeParent(address);
+      await tx.wait();
+      await switchNetwork("11155111");
+      const userAlice = await getUser(signer, owner);
+      const addedDelegate = await userAlice.channel.delegate.add(
+        `eip155:11155111:${address}`
+      );
+      const title = "Parent Removed!!";
+      const body = `${address} successfully removed from Wallet!`;
+      await sendNotification(userAlice, title, body);
+      toast.success("send successfully");
+    } catch (error) {
+      toast.error(error.reason);
+      console.log(error);
+      return;
+    }
+    toast.success("Parent Removed Successfully");
+  }
   return (
     <div>
       <div className="input-group mb-1">
@@ -46,9 +73,12 @@ export default function AddOwner() {
           className="form-control custom-input"
         />
       </div>
-      <div className="input-group mb-1">
+      <div className="input-group mb-1 d-flex justify-content-between">
         <button className="btn btn-danger add-btn" onClick={addOwner}>
           Add
+        </button>
+        <button className="btn btn-danger add-btn" onClick={removeOwner}>
+          Remove
         </button>
       </div>
     </div>
